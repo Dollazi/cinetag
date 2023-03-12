d790486f695f70f621afc7a90f059359897e2e2b
Luis Roberto Moreno Dollazi <luis.moreno.dol@gmail.com>
2023-03-11 23:04:43 -0300

    criação do Banner

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,12 @@
 import Rodape from "components/Rodape"
 import Cabecalho from "components/Cabecalho"
+import Banner from "components/Banner"
 
 function Inicio() {
     return (
         <>
             <Cabecalho />
+            <Banner imagem="home"/>
             <Rodape />
         </>
     )

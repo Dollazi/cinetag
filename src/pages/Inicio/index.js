244dad8c0b8efa2980dfc4e7f5ade7033e772bde
Luis Roberto Moreno Dollazi <luis.moreno.dol@gmail.com>
2023-03-11 23:33:45 -0300

    criação do Card

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -2,6 +2,7 @@ import Rodape from "components/Rodape"
 import Cabecalho from "components/Cabecalho"
 import Banner from "components/Banner"
 import Titulo from "components/Titulo"
+import Card from "components/Card"
 
 function Inicio() {
     return (
@@ -11,6 +12,7 @@ function Inicio() {
             <Titulo>
                 <h1>Um lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
+            <Card id='1' titulo='Filme X' capa='https://br.web.img3.acsta.net/pictures/22/06/08/00/12/0256143.jpg' />
             <Rodape />
         </>
     )
